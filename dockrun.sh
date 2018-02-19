53c6823af2060f99b41c3bd387daa9c149236acd
yagosys <yagosys@gmail.com>
2018-02-19 09:36:03 +0800

    first commit

---
+++ dockrun.sh
@@ -0,0 +1,14 @@
+docker stop vcpe1
+docker rm vcpe1
+docker run -it -d --privileged -v /lib/modules:/lib/modules:ro -p 8123:8123 --name vcpe1 interbeing/vcpe /bin/ash 
+docker exec -it vcpe1 bin/ash -c 'echo 'andy010' > /usr/local/etc/vnet/sn'
+docker exec -it vcpe1 bin/ash -c 'echo 120.55.58.18 core >> /etc/hosts'
+docker exec -it vcpe1 bin/ash -c 'supervisord -c /etc/supervisord.conf '
+docker exec -it vcpe1 bin/ash -c 'ipsec start'
+sleep 1
+vap=`docker exec -it vcpe1 bin/ash -c 'ipsec status' | grep ROUTED | awk '{print $1}'  | cut -d '{' -f 1`
+echo vap is $vap ----before restart ipsec
+docker exec -it vcpe1 bin/ash -c 'ipsec up '$vap''
+docker exec -it vcpe1 bin/ash -c 'ipsec restart'
+echo vap is $vap ----after restart ipsec 
+docker exec -it vcpe1 bin/ash -c 'ipsec up '$vap''
