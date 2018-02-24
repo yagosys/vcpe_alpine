dfc9166543ca6e174c1bf8c9bf8fca6a4d0c4065
your name <your mail>
2018-02-24 15:02:49 +0800

    first commit

--- run_debian.sh
+++ run_debian.sh
@@ -9,7 +9,7 @@ docker network create -d bridge --subnet 172.1.$id.0/24 isolated_nw_$id
 docker run -it -d --privileged --cap-add=ALL --network=isolated_nw_$id -v /lib/modules:/lib/modules:ro -p 81$id:8123 --env ROOT_PASSWORD=MyRootPW123 --env CANALBOX_SN=$sn --env CORE='120.55.58.18' --env FRP_SSH_PORT=62$id --env FRP_POLIPO_PORT=63$id --name $os$sn  interbeing/vcpe_$os
 docker exec -it $os$sn bin/sh -c 'echo "$CANALBOX_SN" > /usr/local/etc/vnet/sn'
 docker exec -it $os$sn bin/sh -c 'echo "$CORE" core >> /etc/hosts'
-docker exec -it $os$sn bin/sh -c 'service ipsec start'
+#docker exec -it $os$sn bin/sh -c 'service ipsec start'
 docker exec -it $os$sn bin/sh -c 'sed -i "s/6210/"$FRP_SSH_PORT"/g" /etc/frpc.ini'
 docker exec -it $os$sn bin/sh -c 'sed -i "s/6310/"$FRP_POLIPO_PORT"/g" /etc/frpc.ini'
 docker exec -it $os$sn bin/sh -c 'sed -i "s/172.17.0.2/172.1.'$id'.2/g" /etc/canalbox.conf'
@@ -35,8 +35,9 @@ fi
 }
 
 get_vap
-until  [[ $vap = *"vap-"* ]] ; do
-	restart_ipsec
-         done
+#until  [[ $vap = *"vap-"* ]] ; do
+#	restart_ipsec
+#docker exec -it $os$sn bin/sh -c 'ipsec status''
+#         done
 
 
